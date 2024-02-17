7aab1f9dcb09c2c3299f7bdec21184755d8657e3
vanshika <vanshikaeumind@gmail.com>
2024-02-17 10:03:55 +0530

    changes made to omkar branch added sos routes

--- backend/express/routes/sheSafeRoutes.js
+++ backend/express/routes/sheSafeRoutes.js
@@ -1,14 +1,35 @@
 const express = require("express");
 const router = express.Router();
 const SafeController = require("../Controllers/SheSafeController");
+const SOS = require("../Controllers/SOS");
 
-//This route will give all books
 router.get("/", (req, res) => {
-    res.send("Hello World");
-})
+  res.send("Hello World");
+});
 router.get("/police", SafeController.policeNearby);
 router.get("/metro", SafeController.MetroNearby);
 router.get("/hospitals", SafeController.HospitalsNearby);
 router.get("/hotels", SafeController.HotelsNearby);
+router.get("/initiate-call-and-msg", async (req, res, next) => {
+  try {
+    // Call voiceTwilio function
+    await SOS.voiceTwilio(req, res, next);
+
+    // Call WhatsappMessage function
+    await SOS.sendWhatsapp(req, res, next);
+
+    // Call WhatsappMessage function
+    await SOS.sendMessage(req, res, next);
+
+    // Send a response indicating both actions were triggered
+    res
+      .status(200)
+      .json({ message: "Voice call and message initiated successfully" });
+  } catch (error) {
+    // Handle errors
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 module.exports = router;
