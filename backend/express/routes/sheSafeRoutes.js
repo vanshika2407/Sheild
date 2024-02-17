7d0e2d66523de28b90559d90f3b289cb4fc4dde6
Omkar Bhoir <71968255+omkar787@users.noreply.github.com>
2024-02-17 11:54:50 +0530

    sos

--- backend/express/routes/sheSafeRoutes.js
+++ backend/express/routes/sheSafeRoutes.js
@@ -12,14 +12,26 @@ router.get("/hospitals", SafeController.HospitalsNearby);
 router.get("/hotels", SafeController.HotelsNearby);
 router.get("/initiate-call-and-msg", async (req, res, next) => {
   try {
+    const name = req.query.name;
+    const lat = req.query.latitude;
+    const lng = req.query.longitude;
+    const client = require('twilio')("ACb0bd2bde89479e44753306098a58d7b3", "e2e9396785dd81ac2df7ecfaf3c02c95");
+
+    // const client = require("twilio")(accountSid, authToken);
+    return await client.messages.create({
+      body: `EMERGENCY! ${name} need help!. Please send assistance immediately. Thank you!.`,
+      from: "whatsapp:+14155238886",
+      to: "whatsapp:+919545491506",
+      persistentAction: `geo:${lat},${lng}|${name}`,
+    });
     // Call voiceTwilio function
-    await SOS.voiceTwilio(req, res, next);
+    // await SOS.voiceTwilio(req, res, next);
 
-    // Call WhatsappMessage function
-    await SOS.sendWhatsapp(req, res, next);
+    // // Call WhatsappMessage function
+    // await SOS.sendWhatsapp(req, res, next);
 
-    // Call WhatsappMessage function
-    await SOS.sendMessage(req, res, next);
+    // // Call WhatsappMessage function
+    // await SOS.sendMessage(req, res, next);
 
     // Send a response indicating both actions were triggered
     res
