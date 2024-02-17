f6761cad86847e099b4f197bbbdf3370df798a6a
Prathamesh Mundada <mundadaprathamesh@gmail.com>
2024-02-17 11:34:59 +0530

    options added

--- backend/express/routes/routesRouter.js
+++ backend/express/routes/routesRouter.js
@@ -1,162 +1,157 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const { Client } = require("@googlemaps/google-maps-services-js");
-const axios = require('axios');
+const axios = require("axios");
 
-const client = new Client({})
-
+const client = new Client({});
 
 router.get("/", async (req, res) => {
-    const origin = req.query.origin;
-    const destination = req.query.destination;
-    const routes = await getRoutes(origin, destination);
+  const origin = req.query.origin;
+  const destination = req.query.destination;
+  const routes = await getRoutes(origin, destination);
 
+  console.log(routes);
+  try {
+    const response = await axios.post(
+      "http://127.0.0.1:8088/safeScore",
+      routes
+    );
+    console.log(response.data);
+    res.json({ api: response.data, routes: routes });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 
-
-    try {
-        const response = await axios.post('http://127.0.0.1:8088/safeScore', routes);
-        console.log(response.data);
-        res.json({ api: response.data, routes: routes });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-
-
-
-    // res.json(response.data);
+  // res.json(response.data);
 });
 
 const getRoutes = async (origin, destination) => {
+  try {
+    const dt = await client.directions({
+      params: {
+        origin: origin,
+        destination: destination,
+        key: "AIzaSyD5fvYs9ENbAEbYROVycp3eVnkiEDceor0",
+        alternatives: true,
+        mode: ["driving", "walking", "transit"],
+      },
+      timeout: 1000,
+    });
+    const data = dt.data;
 
-    try {
+    // Initialize groupedSteps array to store the grouped steps for each route
+    const groupedSteps = [];
 
+    // Iterate over each route
+    for (let i = 0; i < data.routes.length; i++) {
+      const route = data.routes[i];
+      // console.log(route.legs[0]);
+      console.log("--");
+      console.log(route.overview_polyline);
+      console.log("--");
+      const steps = route.legs[0].steps;
+      let currentGroup = [];
+      let currentDistance = 0;
+      const routeGroupedSteps = []; // Grouped steps for the current route
+      let avg_lat = 0;
+      let avg_long = 0;
 
-        const dt = await client.directions({
-            params: {
-                origin: origin,
-                destination: destination,
-                key: "AIzaSyD5fvYs9ENbAEbYROVycp3eVnkiEDceor0",
-                alternatives: true,
-                mode: ["driving", "walking", "transit"],
-            },
-            timeout: 1000
-        })
-        const data = dt.data;
+      // Iterate over each step in the route
+      for (let j = 0; j < steps.length; j++) {
+        const step = steps[j];
+        const stepDistance = step.distance.value;
 
-
-        // Initialize groupedSteps array to store the grouped steps for each route
-        const groupedSteps = [];
-
-        // Iterate over each route
-        for (let i = 0; i < data.routes.length; i++) {
-            const route = data.routes[i];
-            // console.log(route.legs[0]);
-            console.log("--")
-            console.log(route.overview_polyline);
-            console.log("--")
-            const steps = route.legs[0].steps;
-            let currentGroup = [];
-            let currentDistance = 0;
-            const routeGroupedSteps = []; // Grouped steps for the current route
-            let avg_lat = 0;
-            let avg_long = 0;
-
-            // Iterate over each step in the route
-            for (let j = 0; j < steps.length; j++) {
-                const step = steps[j];
-                const stepDistance = step.distance.value;
-
-                if (currentDistance < 1000 || currentDistance + stepDistance <= 1000) {
-                    if (currentGroup.length === 0) {
-                        avg_lat = step.start_location.lat;
-                        avg_long = step.start_location.lng;
-                    }
-                    currentGroup.push(step);
-                    currentDistance += stepDistance;
-                } else {
-                    avg_lat = (avg_lat + step.end_location.lat) / 2;
-                    avg_long = (avg_long + step.end_location.lng) / 2;
-                    currentGroup.push([avg_lat, avg_long]);
-                    routeGroupedSteps.push(currentGroup);
-                    currentGroup = [step];
-                    currentDistance = stepDistance;
-                }
-            }
-
-            // Handle the last group
-            if (currentGroup.length > 0) {
-                avg_lat = currentGroup[0].start_location.lat;
-                avg_long = currentGroup[0].start_location.lng;
-                const lastStep = currentGroup[currentGroup.length - 1];
-                const avg_lat_1 = lastStep.end_location.lat;
-                const avg_long_1 = lastStep.end_location.lng;
-                currentGroup.push([(avg_lat_1 + avg_lat) / 2, (avg_long_1 + avg_long) / 2]);
-                routeGroupedSteps.push(currentGroup);
-            }
-
-            // Push the grouped steps for the current route to groupedSteps array
-            groupedSteps.push({
-                grouped: routeGroupedSteps,
-                polyline: route.overview_polyline,
-                distance: route.legs[0].distance.text,
-                duration: route.legs[0].duration.text,
-            });
+        if (currentDistance < 1000 || currentDistance + stepDistance <= 1000) {
+          if (currentGroup.length === 0) {
+            avg_lat = step.start_location.lat;
+            avg_long = step.start_location.lng;
+          }
+          currentGroup.push(step);
+          currentDistance += stepDistance;
+        } else {
+          avg_lat = (avg_lat + step.end_location.lat) / 2;
+          avg_long = (avg_long + step.end_location.lng) / 2;
+          currentGroup.push([avg_lat, avg_long]);
+          routeGroupedSteps.push(currentGroup);
+          currentGroup = [step];
+          currentDistance = stepDistance;
         }
+      }
 
-        // Now groupedSteps contains grouped steps for each route
+      // Handle the last group
+      if (currentGroup.length > 0) {
+        avg_lat = currentGroup[0].start_location.lat;
+        avg_long = currentGroup[0].start_location.lng;
+        const lastStep = currentGroup[currentGroup.length - 1];
+        const avg_lat_1 = lastStep.end_location.lat;
+        const avg_long_1 = lastStep.end_location.lng;
+        currentGroup.push([
+          (avg_lat_1 + avg_lat) / 2,
+          (avg_long_1 + avg_long) / 2,
+        ]);
+        routeGroupedSteps.push(currentGroup);
+      }
 
-
-        // 
-        // const steps = data.routes[0].legs[0].steps;
-        // let currentGroup = [];
-        // let currentDistance = 0;
-        // const groupedSteps = [];
-        // avg_lat = 0
-        // for (let i = 0; i < steps.length; i++) {
-        //     const step = steps[i];
-        //     const stepDistance = step.distance.value;
-
-        //     if (currentDistance < 1000 || currentDistance + stepDistance <= 1000) {
-        //         if (currentGroup.length == 0) {
-        //             avg_lat = step.start_location.lat
-        //             avg_long = step.start_location.lng
-        //         }
-        //         currentGroup.push(step);
-        //         currentDistance += stepDistance;
-        //     } else {
-
-        //         avg_lat = (avg_lat + step.end_location.lat) / 2
-        //         avg_long = (avg_long + step.end_location.lng) / 2
-        //         currentGroup.push([avg_lat, avg_long])
-
-        //         groupedSteps.push(currentGroup);
-        //         currentGroup = [step];
-        //         currentDistance = stepDistance;
-        //     }
-        // }
-
-        // if (currentGroup.length > 0) {
-        //     avg_lat = currentGroup[0].start_location.lat
-        //     avg_long = currentGroup[0].start_location.lng
-
-
-        //     avg_lat_1 = currentGroup[currentGroup.length - 1].end_location.lat
-        //     avg_long_1 = currentGroup[currentGroup.length - 1].end_location.lng
-
-        //     currentGroup.push([(avg_lat_1 + avg_lat) / 2, (avg_long_1 + avg_long) / 2])
-        //     groupedSteps.push(currentGroup);
-        // }
-
-        // console.log(data)
-        // fs.writeFileSync("data.json", JSON.stringify(data));
-
-        return groupedSteps;
-        // fs.writeFileSync('data.json', JSON.stringify(dt.data));
-    } catch (error) {
-        console.log(error);
+      // Push the grouped steps for the current route to groupedSteps array
+      groupedSteps.push({
+        grouped: routeGroupedSteps,
+        polyline: route.overview_polyline,
+        distance: route.legs[0].distance.text,
+        duration: route.legs[0].duration.text,
+      });
     }
-}
 
+    // Now groupedSteps contains grouped steps for each route
+
+    //
+    // const steps = data.routes[0].legs[0].steps;
+    // let currentGroup = [];
+    // let currentDistance = 0;
+    // const groupedSteps = [];
+    // avg_lat = 0
+    // for (let i = 0; i < steps.length; i++) {
+    //     const step = steps[i];
+    //     const stepDistance = step.distance.value;
+
+    //     if (currentDistance < 1000 || currentDistance + stepDistance <= 1000) {
+    //         if (currentGroup.length == 0) {
+    //             avg_lat = step.start_location.lat
+    //             avg_long = step.start_location.lng
+    //         }
+    //         currentGroup.push(step);
+    //         currentDistance += stepDistance;
+    //     } else {
+
+    //         avg_lat = (avg_lat + step.end_location.lat) / 2
+    //         avg_long = (avg_long + step.end_location.lng) / 2
+    //         currentGroup.push([avg_lat, avg_long])
+
+    //         groupedSteps.push(currentGroup);
+    //         currentGroup = [step];
+    //         currentDistance = stepDistance;
+    //     }
+    // }
+
+    // if (currentGroup.length > 0) {
+    //     avg_lat = currentGroup[0].start_location.lat
+    //     avg_long = currentGroup[0].start_location.lng
+
+    //     avg_lat_1 = currentGroup[currentGroup.length - 1].end_location.lat
+    //     avg_long_1 = currentGroup[currentGroup.length - 1].end_location.lng
+
+    //     currentGroup.push([(avg_lat_1 + avg_lat) / 2, (avg_long_1 + avg_long) / 2])
+    //     groupedSteps.push(currentGroup);
+    // }
+
+    // console.log(data)
+    // fs.writeFileSync("data.json", JSON.stringify(data));
+
+    return groupedSteps;
+    // fs.writeFileSync('data.json', JSON.stringify(dt.data));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = router;
--- lib/features/navigation/routing.dart
+++ lib/features/navigation/routing.dart
@@ -1,19 +1,19 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:google_maps_apis/places.dart';
 import 'package:she_secure/colors.dart';
+import 'package:she_secure/features/navigation/route_tile.dart';
 import '../../keys.dart';
+import 'package:http/http.dart' as http;
 
 class RoutingWidget extends StatefulWidget {
   const RoutingWidget({
     Key? key,
-    this.startLoc,
-    this.destLoc,
   }) : super(key: key);
-  final startLoc;
-  final destLoc;
   static const routeName = '/routing';
 
   @override
@@ -23,29 +23,28 @@ class RoutingWidget extends StatefulWidget {
 class _RoutingwidgetState extends State<RoutingWidget> {
   var sessionToken = 'xyzabc_1234';
   late GoogleMapController mapController;
-  final TextEditingController startController = TextEditingController();
-  final TextEditingController destinationController = TextEditingController();
   Map<String, Marker> _markers = {};
   bool isLoading = false;
   bool isLocationLoading = false;
   var searchedLocation;
 
+  var routes;
+
   @override
-  void initState() {
-    super.initState();
-    startController.text = widget.startLoc["name"];
-    destinationController.text = widget.destLoc["name"];
-    Marker marker = Marker(
-      markerId: MarkerId(""),
-      position: LatLng(widget.startLoc["lat"], widget.startLoc["lng"]),
-    );
-    _markers[widget.startLoc["name"]] = marker;
-    marker = Marker(
-      markerId: MarkerId(""),
-      position: LatLng(widget.destLoc["lat"], widget.destLoc["lng"]),
-    );
-    _markers[widget.destLoc["name"]] = marker;
-  }
+  // void initState() {
+  //   // TODO: implement initState
+  //   super.initState();
+  //   Marker marker = Marker(
+  //     markerId: MarkerId(""),
+  //     position: LatLng(widget.startLoc["lat"], widget.startLoc["lng"]),
+  //   );
+  //   _markers[widget.startLoc["name"]] = marker;
+  //   marker = Marker(
+  //     markerId: MarkerId(""),
+  //     position: LatLng(widget.destLoc["lat"], widget.destLoc["lng"]),
+  //   );
+  //   _markers[widget.destLoc["name"]] = marker;
+  // }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
@@ -59,28 +58,7 @@ class _RoutingwidgetState extends State<RoutingWidget> {
     zoom: 13,
   );
 
-  void _onOptionSelected(String selectedName, double lati, double longi) {
-    setState(() {
-      destinationController.text = selectedName;
-      _markers.clear();
-      final Marker marker = Marker(
-        markerId: MarkerId(""),
-        position: LatLng(lati, longi),
-      );
-      _markers[""] = marker;
-      opt = [];
-      searchedLocation = {
-        "name": selectedName,
-      };
-      double zoomLevel = 15.0;
-      mapController.animateCamera(
-        CameraUpdate.newLatLngZoom(
-          LatLng(lati, longi),
-          zoomLevel,
-        ),
-      );
-    });
-  }
+  
 
   @override
   Widget build(BuildContext context) {
@@ -100,47 +78,6 @@ class _RoutingwidgetState extends State<RoutingWidget> {
                     markers: _markers.values.toSet(),
                   ),
                   if (isLocationLoading)
-                    Center(
-                      child: CircularProgressIndicator(),
-                    ),
-                ],
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-              decoration: BoxDecoration(
-                color: greyColor,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20),
-                  topRight: Radius.circular(20),
-                ),
-              ),
-              child: Column(
-                children: [
-                  TextField(
-                    controller: startController,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      hintText: 'Start Location...',
-                    ),
-                    onChanged: (value) async {
-                      // Handle text field changes
-                    },
-                  ),
-                  SizedBox(height: 10),
-                  TextField(
-                    controller: destinationController,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      hintText: 'Destination...',
-                    ),
-                    onChanged: (value) async {
-                      // Handle text field changes
-                    },
-                  ),
-                  if (isLoading)
                     Center(
                       child: CircularProgressIndicator(),
                     ),
